27a337e36df8aeb7b8b21eb54f8605eee1cc98d6
ata xan <ataa1370@gmail.com>
2024-05-12 10:18:37 +0200

    home page title is done

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,15 @@
 
 function Home() {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Welcome to Promptopia
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">Promptopia is a platform for writing prompts and stories.</p>
+      {/* {Feed} */}
+    </section>
+
   )
 }
 
